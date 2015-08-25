aa95d1f01405519a309ee2ffe71680300b08238a
Prithvi Prabhu <prithvi@0xdata.com>
2015-08-24 20:50:34 -0700

    Add createJob, listJobs, Job to IDL.

--- web/web.thrift
+++ web/web.thrift
@@ -1,6 +1,12 @@
+struct Job {
+  1: required string category;
+  2: required string title;
+}
 service Web {
-    string ping()
-    string echo(1: string message)
-    string predict(1: string jobTitle)
+  string ping();
+  string echo(1: string message);
+  string predict(1: string jobTitle);
+  void createJob(1: Job job);
+  list<Job> listJobs(1: i32 skip, 2: i32 limit);
 }
 
